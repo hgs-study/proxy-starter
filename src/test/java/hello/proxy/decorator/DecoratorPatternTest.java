0c63de14867e9327c74bf563250fb1ce0db14616
hgs-study <hgstudy_@naver.com>
2022-06-03 00:31:18 +0900

    Decorator 패턴 2

--- src/test/java/hello/proxy/decorator/DecoratorPatternTest.java
+++ src/test/java/hello/proxy/decorator/DecoratorPatternTest.java
@@ -1,6 +1,8 @@
 package hello.proxy.decorator;
 
+import hello.proxy.decorator.code.Component;
 import hello.proxy.decorator.code.DecoratorPatternClient;
+import hello.proxy.decorator.code.MessageDecorator;
 import hello.proxy.decorator.code.RealComponent;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -12,6 +14,18 @@ public class DecoratorPatternTest {
     void noDecorator(){
         RealComponent realComponent = new RealComponent();
         DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+        client.execute();
+    }
+
+    /**
+     * 요구사항 : 코드를 수정하지 않고 로그를 남겨야한다.
+     * 데코레이터 추가됨
+     */
+    @Test
+    void decorator1(){
+        Component realComponent = new RealComponent();
+        MessageDecorator messageDecorator = new MessageDecorator(realComponent);
+        DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
         client.execute();
     }
 }
